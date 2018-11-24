d0a03113b53061804caf25bbcfe05397572e02d6
Abdullah Mohammed <abdullah.mohammed@maf.co.ae>
2018-11-24 17:49:36 +0200

    comments

--- packages/encore/laravel-admin/resources/views/filter/select.blade.php
+++ packages/encore/laravel-admin/resources/views/filter/select.blade.php
@@ -1,5 +1,5 @@
 <select class="form-control {{ $class }}" name="{{$name}}" style="width: 100%;">
-    <option></option>
+    <option>all option</option>
     @foreach($options as $select => $option)
         <option value="{{$select}}" {{ (string)$select === request($name, $value) ?'selected':'' }}>{{$option}}</option>
     @endforeach
